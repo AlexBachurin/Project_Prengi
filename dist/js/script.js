afbeeb910de255342555c07a11f6a29560af603e
AlexBach <alexanderbach91@gmail.com>
2021-02-13 19:16:01 +0300

    first blocks plus second slider

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+//slider
+
+const slider = tns({
+    container: '.solutions__slider',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    navContainer: ".solutions__slider-list",
+    controlsContainer: ".solutions__slider-controls",
+    speed: 500
+  });
