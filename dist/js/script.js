7c510d0fc7f3a3d9555ec1a93b5e4718d5efc6c3
Alex <alexbachurin91@gmail.com>
2021-03-06 13:40:27 +0300

    full adaptation

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,37 +1,51 @@
 //slider
 
 const slider = tns({
-    container: '.solutions__slider',
-    items: 1,
-    slideBy: 'page',
-    navContainer: ".solutions__slider-list",
-    controlsContainer: ".solutions__slider-controls",
-    speed: 500,
-    autoplay: true,
-    autoplayButtonOutput: false,
-    mouseDrag: true
+  container: '.solutions__slider',
+  items: 1,
+  slideBy: 'page',
+  navContainer: ".solutions__slider-list",
+  controlsContainer: ".solutions__slider-controls",
+  speed: 500,
+  autoplay: true,
+  autoplayButtonOutput: false,
+  mouseDrag: true
 
-  });
+});
 
-  const sliderMain = tns({
-    container: '.slider',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    navContainer: ".slider__dots",
-    controlsContainer: ".slider__controls",
-    speed: 500,
-    autoplay: true,
-    autoplayButtonOutput: false,
-    // gutter: 20
-  });
+const sliderMain = tns({
+  container: '.slider',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  navContainer: ".slider__dots",
+  controlsContainer: ".slider__controls",
+  speed: 500,
+  autoplay: true,
+  autoplayButtonOutput: false,
+  // gutter: 20
+});
 
-  //menu
+//menu
 
-  const hamburger = document.querySelector('.hamburger');
-  const menu = document.querySelector('.head__menu')
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.head__menu'),
+  links = document.querySelectorAll('.head__link'),
+  overlay = document.querySelector('.head__overlay')
 
-  hamburger.addEventListener('click', () => {
-    menu.classList.toggle('active')
-    hamburger.classList.toggle('is-active')
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('active')
+  hamburger.classList.toggle('is-active')
+})
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    menu.classList.remove('active')
+    hamburger.classList.remove('is-active')
   })
+});
+
+overlay.addEventListener('click', () => {
+  menu.classList.remove('active')
+  hamburger.classList.remove('is-active')
+})
